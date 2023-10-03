12174511b4886e550de16b086c6ad4c02168db1d
Santiago Benitez <santibenitez2373@gmail.com>
2023-10-03 14:21:52 -0500

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Santiago Benitez on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
